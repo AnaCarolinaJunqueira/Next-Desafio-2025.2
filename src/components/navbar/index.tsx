c13757b984b0c2b935541186a3025454146f1e0b
Ana Carolina Junqueira e Silva <acjunqueiras@gmail.com>
2025-08-25 04:01:35 -0300

    arrumando a centralização

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -23,23 +23,24 @@ export default function Header(){
 
     const toggleNav = () => setIsNavOpen(!isNavOpen)
     return(
-        <header className="bg-darker-midnight-blue stcky top-0 z-20 mx-auto w-full py-4 px-6 md:p-0 mb-8">
+        <header className="bg-darker-midnight-blue stcky top-0 z-20 mx-auto w-full py-4 px-6 bp-840:p-0 mb-8">
 
-            <nav className="flex items-center justify-between w-full md:w-11/12 mx-auto">
-            <div className="flex items-center justify-between w-full">
-            {/*Logo*/}
-            <Link href="/" className="flex gap-4 items-center">
-                <Image
-                src={'/logo/logo2.png'}
-                alt="Logo do site"
-                width={940}
-                height={940}
-                className="h-30 w-30 rounded-xl"
-                />
-                <span className="text-white hidden md:block text-[20px] font-semibold">Covil das Cartas</span>
-            </Link>
+            <nav className="flex items-center justify-between w-full bp-840:w-11/12 mx-auto">
+            <div className="flex justify-between mx-auto">
+                {/*Logo*/}
+                <Link href="/" className="flex gap-4 items-center">
+                    <Image
+                    src={'/logo/logo2.png'}
+                    alt="Logo do site"
+                    width={940}
+                    height={940}
+                    className="h-30 w-30 rounded-xl"
+                    />
+                    <span className="text-white hidden bp-840:block text-[20px] font-semibold">Covil das Cartas</span>
+                </Link>
+            </div>
                 {/*Links do meio */}
-                <div className="flex-1 hidden px-50 gap-10 md:flex">
+                <div className="flex-1 hidden justify-center gap-8 bp-840:flex">
                     {links1.map((link, index) =>
                     <Link href={link.href} key={index}>
                     <span className="text-lg text-white hover:bg-white/20 p-2 rounded-x1">{link.label}</span>
@@ -48,7 +49,7 @@ export default function Header(){
                 </div>
 
                 {/*Links da direita*/}
-                <div className="hidden md:flex gap-6 items-center">
+                <div className="hidden bp-840:flex justify-center gap-6 items-center w-[282px] h-[120px]">
                     {links2.map((link, index) =>{
                         const Icon = link.icone;
                         return(
@@ -60,15 +61,14 @@ export default function Header(){
                     })}
                 </div>
                 {/*menu hamburger */}
-                <button className="md:hidden text-white" onClick={toggleNav}>
+                <button className="bp-840:hidden text-white" onClick={toggleNav}>
                     <AlignJustify size={40}/>
                 </button>
-                </div>
             </nav>
 
             {/*Menu Hamburger Aberto */}
             {isNavOpen && (
-                <div className="md:hidden flex flex-col items-center justify-center mt-4 gap-4">
+                <div className="bp-840:hidden flex flex-col items-center justify-center mt-4 gap-4">
                     {links1.map((link, index) => (
                         <Link href={link.href} key={index} className="flex justify-center w-full text-center text-white px-4 py-2 rounded-x1 hover:bg-gray-700">
                         <span className="text-lg">
