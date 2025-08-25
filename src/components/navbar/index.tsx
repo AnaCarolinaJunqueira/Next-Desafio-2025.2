785347f5672061020c56a0519c87ed1e91cc74d7
Ana Carolina Junqueira e Silva <acjunqueiras@gmail.com>
2025-08-25 02:38:47 -0300

    adicionando os links na navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,14 +5,16 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
-const links = [
+const links1 = [
     {href: '/', label: 'Home'},
     {href: '/produtos', label: 'Produtos'},
     {href: '/contato', label: 'Contato'},
-    {href: '/gerenciamento', label: 'Gerenciamento de Produtos'},
-    {href: '/login', label: 'Login', icone: CircleUserRound},
+    {href: '/gerenciamento', label: 'Gerenciamento'}   
+]
+
+const links2 = [
+    {href: '/login', label: '', icone: CircleUserRound},
     {href: '/carrinho', label: '', icone: ShoppingCart}
-    
 ]
 
 export default function Header(){
@@ -20,17 +22,43 @@ export default function Header(){
 
     const toggleNav = () => setIsNavOpen(!isNavOpen)
     return(
-        <header className="bg-darker-midnight-blue stcky top-0 z-20 mx-auto w-full py-8 px-4 md:p-0 mb-8">
-            <div className="flex flex-wrap items-center justify-between w-full md:w-10/12 mx-auto">
+        <header className="bg-darker-midnight-blue stcky top-0 z-20 mx-auto w-full py-4 px-6 md:p-0 mb-8">
+            <div className="flex items-center justify-between w-full md:w-11/12 mx-auto">
+            {/*Logo*/}
             <Link href="/" className="flex gap-4 items-center">
                 <Image
                 src={'/logo/logo2.png'}
                 alt="Logo do site"
-                width={904}
-                height={904}
+                width={940}
+                height={940}
                 className="h-30 w-30 rounded-xl"
                 />
+                <span className="text-white hidden md:block text-[20px] font-semibold">Covil das Cartas</span>
             </Link>
+
+            <nav className="flex items-center justify-between w-full md:w-11/12 mx-auto">
+                {/*Links do meio */}
+                <div className="flex-1 hidden justify-center gap-8 md:flex">
+                    {links1.map((link, index) =>
+                    <Link href={link.href} key={index}>
+                    <span className="text-3x1 text-white hover:bg-white/20 p-2 rounded-x1">{link.label}</span>
+                    </Link>
+                    )}
+                </div>
+
+                {/*Links da direita*/}
+                <div className="hidden md:flex gap-6 items-center">
+                    {links2.map((link, index) =>{
+                        const Icon = link.icone;
+                        return(
+                        <Link className="flex items-center gap-1" href={link.href} key={index}>
+                            {Icon && <Icon size={24} className="text-white"/>}
+                            {link.label && <span className="text-white">{link.label}</span>}
+                        </Link>
+                        );
+                    })}
+                </div>
+            </nav>
             </div>
 
         </header>
