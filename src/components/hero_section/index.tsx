efc95604080a189df96001a66d77b6555b13b0e3
Ana Carolina Junqueira e Silva <anacarolina.junqueira@gmail.com>
2025-09-17 02:14:01 -0300

    arrumando os botões e texto da hero section

--- src/components/hero_section/index.tsx
+++ src/components/hero_section/index.tsx
@@ -1,14 +1,16 @@
 export default function HeroSection(){
     return(
-        <div className="flex items-center justify-center bg-cover h-[600px] bg-center text-white padding-0" style={{backgroundImage: "url('/home_page/herobg.jpeg')"}}>
-            <h1>Qualidade, proteção e confiança em cada carta</h1>
-            <h2 className="text-center">Produtos bem cuidados, lacrados e protegidos - prontos para sua coleção ou seu deck competitivo. Enviamos cartas em português, inglês e japonês com total segurança.</h2>
-            <div >
-                <button>Ver Produtos</button>
-               <button className="hidden">Yu-Gi-Oh!</button>
-               <button className="hidden">Pokémon</button>
-               <button className="hidden">Digimon</button>
-               <button className="hidden">Acessórios</button> 
+        <div className="flex items-center justify-center bg-cover h-[550px] md:h-[650px] bg-center text-white padding-0" style={{backgroundImage: "url('/home_page/herobg.jpeg')"}}>
+            <div className="z-10 text-center px-4 max-w-2xl md:max-w-5xl">
+                <h1 className="text-4xl md:text-6xl font-bold mb-7">Qualidade, proteção e confiança em cada carta</h1>
+                <h2 className="text-lg font-light md:text-xl mb-20">Produtos bem cuidados, lacrados e protegidos - prontos para sua coleção ou seu deck competitivo. Enviamos cartas em português, inglês e japonês com total segurança.</h2>
+            <div className="flex justify-center gap-20">
+                <button className="px-6 py-3 text-xl bg-blue-magenta hover:bg-darker-blue-magenta rounded-lg font-semibold shadow-lg lg:hidden">Ver Produtos</button>
+               <button className="hidden lg:inline-block px-6 py-3 text-2xl bg-blue-magenta hover:bg-darker-blue-magenta rounded-lg font-semibold shadow-lg">Yu-Gi-Oh!</button>
+               <button className="hidden lg:inline-block px-6 py-3 text-2xl bg-blue-magenta hover:bg-darker-blue-magenta rounded-lg font-semibold shadow-lg">Pokémon</button>
+               <button className="hidden lg:inline-block px-6 py-3 text-2xl bg-blue-magenta hover:bg-darker-blue-magenta rounded-lg font-semibold shadow-lg">Digimon</button>
+               <button className="hidden lg:inline-block px-6 py-3 text-2xl bg-blue-magenta hover:bg-darker-blue-magenta rounded-lg font-semibold shadow-lgs">Acessórios</button>
+            </div>
             </div>
         </div>
     )
