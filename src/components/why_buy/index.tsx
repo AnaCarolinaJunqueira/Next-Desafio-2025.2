57af57c30ae88784b80cf1636e81d975009ce572
Ana Carolina Junqueira e Silva <anacarolina.junqueira@gmail.com>
2025-09-19 00:16:20 -0300

    seção por que comprar conosco pronta

--- src/components/why_buy/index.tsx
+++ src/components/why_buy/index.tsx
@@ -1,15 +1,18 @@
 import Title from "../title";
 import WhyBuyCard from "./card";
-import {Package} from "lucide-react";
+import {Package, BadgeCheck, ShieldCheck, Truck} from "lucide-react";
 
 export default function WhyBuy(){
     return(
         <div className="w-full space-y-12">
             {/*Titulo da seção */}
             <Title title="Por que comprar conosco?"/>
-            <div className="flex flex-wrap gap-14 md:gap-20 justify-center">
+            <div className="flex flex-wrap gap-14 md:gap-20 justify-center pb-20">
                 {/*Cards*/}
                 <WhyBuyCard icon={Package} title="Embalagem com proteção reforçada"/>
+                <WhyBuyCard icon={BadgeCheck} title="Cartas 100% originais"/>
+                <WhyBuyCard icon={ShieldCheck} title="Pagamento Seguro"/>
+                <WhyBuyCard icon={Truck} title="Envio rápido e rastreável"/>
             </div>
         </div>
     )
